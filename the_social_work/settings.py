bb0bd4b6bacd427c4aa0c0d5b6fd7e40f1519943
amrshedou <amrshedou@gmail.com>
2022-01-23 06:55:51 +0200

    new changes

--- the_social_work/settings.py
+++ the_social_work/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': 'helper',                     
         'USER': 'myuser',
         'PASSWORD': 'password',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'HOST': 'help-er2.herokuapp.com',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
